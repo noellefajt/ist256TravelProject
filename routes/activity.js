5c007b8cd2903e42affff015821749f7100a1c35
VinceSemrau <vincesemrau@gmail.com>
2021-04-12 19:22:09 -0400

    npm working again, landmark api set up but not working

--- routes/activity.js
+++ routes/activity.js
@@ -2,12 +2,13 @@ var axios = require('axios').default;
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
     
-    axios.get('https://opendata.arcgis.com/datasets/7a7aca614ad740e99b060e0ee787a228_3.geojson').then(function(response){
+    axios.get('https://gis.cityofboston.gov/arcgis/rest/services/EnvironmentEnergy/OpenData/MapServer/3/query?where=1%3D1&outFields=*&outSR=4326&f=json').then(function(response){
         res.send(response.data);
-        console.log(response);
         console.log(response.data);
+        
     });
 });
 
