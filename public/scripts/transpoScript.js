54bf5a8a2b40d5d6b2cb1df5f360e0d202143c48
karina san roman <kds5612@psu.edu>
2021-04-12 23:20:53 -0400

    more from today

--- public/scripts/transpoScript.js
+++
@@ -1,9 +0,0 @@
-console.log("script file loaded");
-
-function sayHello(){
-    let myMessage = document.getElementById("message");
-    myMessage.textContent = "hello world";
-}
-
-const helloButton = document.getElementById("button");
-helloButton.addEventListener("click", sayHello); 
