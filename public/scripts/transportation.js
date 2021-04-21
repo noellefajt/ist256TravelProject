0d03ba32fb32fc1f27e2d1217d615b617d2affc7
karina san roman <kds5612@psu.edu>
2021-04-21 19:41:02 -0400

    created table to disply the data of the stops and times for subway

--- public/scripts/transportation.js
+++ public/scripts/transportation.js
@@ -27,12 +27,7 @@ const data = {
                     "11:04pm"
                 ]
             },
-            {
-                "stopName": "Hynes Convention Center",
-                "time": [
-                    "11:06pm"
-                ]
-            },
+            
         ],
 
         "orangeLine": [
@@ -119,9 +114,13 @@ const data = {
 
     }
 }
+
+
+
+
 //fucntion handles a green button click 
 function greenClick() {
-
+    document.getElementById("table")
     let greenStop = document.getElementById("lineStop");
     let greenTime = document.getElementById("lineTime");
     let greenImg = document.getElementById("linePhoto");
@@ -133,12 +132,14 @@ function greenClick() {
         //prints out stop name 
         let p = document.createElement("p")
         let pText = document.createTextNode(stop.stopName);
+        p.classList.add("border");
         p.appendChild(pText);
         greenStop.appendChild(p);
 
         //prints out stop time
         let t = document.createElement("p");
         let tText = document.createTextNode(stop.time);
+        t.classList.add("border");
         t.appendChild(tText);
         greenTime.appendChild(t);
     })
@@ -150,72 +151,92 @@ function greenClick() {
 }
 document.getElementById("greenButton").addEventListener("click", greenClick);
 
-
 //function handles orange button clicks  
 function orangeClick() {
-    let orangeData = document.getElementById("lineData");
-    orangeData.innerHTML = null;
+    let orangeStop = document.getElementById("lineStop");
+    let orangeTime = document.getElementById("lineTime");
+    let orangeImg = document.getElementById("linePhoto");
+    orangeStop.innerHTML = null;
+    orangeTime.innerHTML = null;
+    orangeImg.innerHTML = null;
+
     data.subwayData.orangeLine.forEach(stop => {
         let p = document.createElement("p");
         let pText = document.createTextNode(stop.stopName);
+        p.classList.add("border");
         p.appendChild(pText);
-        orangeData.appendChild(p);
+        orangeStop.appendChild(p);
 
         //prints out stop time
-        let t = document.createElement("t");
+        let t = document.createElement("p");
         let tText = document.createTextNode(stop.time);
+        t.classList.add("border");
         t.appendChild(tText);
-        orangeData.appendChild(t);
+        orangeTime.appendChild(t);
     })
 
     let img = document.createElement("img");
     img.src = "images/subwaylineorange.png";
-    orangeData.appendChild(img);
+    orangeImg.appendChild(img);
 }
 document.getElementById("orangeButton").addEventListener("click", orangeClick);
 
 //function handles blue button click 
 function blueClick() {
-    let blueData = document.getElementById("lineData");
-    blueData.innerHTML = null;
+    let blueStop = document.getElementById("lineStop");
+    let blueTime = document.getElementById("lineTime");
+    let blueImg = document.getElementById("linePhoto");
+    blueStop.innerHTML = null;
+    blueTime.innerHTML = null;
+    blueImg.innerHTML = null;
+
     data.subwayData.blueLine.forEach(stop => {
         let p = document.createElement("p");
         let pText = document.createTextNode(stop.stopName);
+        p.classList.add("border");
         p.appendChild(pText);
-        blueData.appendChild(p);
+        blueStop.appendChild(p);
 
         //prints out stop time
-        let t = document.createElement("t");
+        let t = document.createElement("p");
         let tText = document.createTextNode(stop.time);
+        t.classList.add("border");
         t.appendChild(tText);
-        blueData.appendChild(t);
+        blueTime.appendChild(t);
     })
 
     let img = document.createElement("img");
     img.src = "images/subwaylineblue.png";
-    blueData.appendChild(img);
+    blueImg.appendChild(img);
 }
 document.getElementById("blueButton").addEventListener("click", blueClick);
 
 //function handles red button click 
 function redClick() {
-    let redData = document.getElementById("lineData");
-    redData.innerHTML = null;
+   let redStop = document.getElementById("lineStop");
+    let redTime = document.getElementById("lineTime");
+    let redImg = document.getElementById("linePhoto");
+    redStop.innerHTML = null;
+    redTime.innerHTML = null;
+    redImg.innerHTML = null;
+
     data.subwayData.redLine.forEach(stop => {
         let p = document.createElement("p");
         let pText = document.createTextNode(stop.stopName);
+        p.classList.add("border");
         p.appendChild(pText);
-        redData.appendChild(p);
+        redStop.appendChild(p);
 
         //prints out stop time
-        let t = document.createElement("t");
+        let t = document.createElement("p");
         let tText = document.createTextNode(stop.time);
+        t.classList.add("border");
         t.appendChild(tText);
-        redData.appendChild(t);
+        redTime.appendChild(t);
     })
 
     let img = document.createElement("img");
     img.src = "images/subwayMapRed.png";
-    redData.appendChild(img);
+    redImg.appendChild(img);
 }
 document.getElementById("redButton").addEventListener("click", redClick);
